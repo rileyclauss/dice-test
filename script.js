8aeca1c947006e608003d9e008e43c43a291b89b
rileyclauss <riley.clauss@gmail.com>
2020-05-27 15:29:48 -0400

    displaying bootstrap graph

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-var rolls = []
+var rolls = [10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10]
 
 function init(){
   document.getElementById("rollBox")
@@ -9,7 +9,43 @@ function init(){
         document.getElementById("rollButton").click();
     }
   });
-  
+  var ctx = document.getElementById("myChart").getContext('2d');
+  var myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+      labels: ["1", "2", "3", "4", "5", "6", "7", "8", "9","10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20"],
+    datasets: [{
+      label: '# of Rolls',
+      data: rolls,
+      backgroundColor: [
+      'rgba(255, 99, 132, 0.2)',
+      'rgba(54, 162, 235, 0.2)',
+      'rgba(255, 206, 86, 0.2)',
+      'rgba(75, 192, 192, 0.2)',
+      'rgba(153, 102, 255, 0.2)',
+      'rgba(255, 159, 64, 0.2)'
+      ],
+      borderColor: [
+      'rgba(255,99,132,1)',
+      'rgba(54, 162, 235, 1)',
+      'rgba(255, 206, 86, 1)',
+      'rgba(75, 192, 192, 1)',
+      'rgba(153, 102, 255, 1)',
+      'rgba(255, 159, 64, 1)'
+      ],
+      borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        yAxes: [{
+          ticks: {
+            beginAtZero: true
+          }
+        }]
+      }
+    }
+  });
 }
 
 function generateMenu(){
